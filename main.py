11ddc368ec559f7daa86b7b333614702416cc768
tjs322 <tjs322@cornell.edu>
2024-11-16 14:33:06 -0500

    Create print statements to display retrieved data.

---
+++ main.py
@@ -0,0 +1,24 @@
+import sports
+import retrieve
+
+if __name__ == '__main__':
+  #NFL Display
+  print("NFL Display")
+  week = 9
+  nfl = sports.NFL(week)
+  df = retrieve.SportsbookReviewAPI(nfl.money_line, 'Money Line', 'Week', week).return_data()
+  print(df)
+
+  #NHL Display
+  print("NHL Display")
+  date = '2024-11-15'
+  nhl = sports.NHL(date)
+  df = retrieve.SportsbookReviewAPI(nhl.money_line, 'Money Line', 'Date', date).return_data()
+  print(df)
+
+  #NBA Display
+  print("NBA Display")
+  date = '2024-11-15'
+  nba = sports.NBA(date)
+  df = retrieve.SportsbookReviewAPI(nba.money_line, 'Money Line', 'Date', date).return_data()
+  print(df)
